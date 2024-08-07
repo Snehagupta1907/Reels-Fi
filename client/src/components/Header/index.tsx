4d6d21c28b696ca4bb826866f6534d091f9184ea
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2024-08-08 01:16:27 +0530

    UPdate

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,12 +1,13 @@
 "use client";
 import React from 'react';
 import ChainDropdown from '../ChainDropdown';
-
+import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
 const Header = () => {
   return (
     <header className="bg-dark-2 py-3 px-6 flex items-center justify-between shadow-md sticky top-0 z-30">
       <div className="flex-grow"></div>
-      <div className="relative">
+      <div className="relative flex gap-5">
+        <WalletSelector />
         <ChainDropdown />
       </div>
     </header>
