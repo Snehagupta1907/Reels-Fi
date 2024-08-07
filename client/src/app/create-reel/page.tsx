b18ead4cefc78af2db6b9a857d554523b1d2e4c6
Snehagupta1907 <snehagupta98930@gmail.com>
2024-08-08 00:02:24 +0530

    update

--- client/src/app/create-reel/page.tsx
+++ client/src/app/create-reel/page.tsx
@@ -23,7 +23,6 @@ const CreateReelPage = () => {
       monetize,
     });
   };
-
   return (
     <div className="min-h-screen bg-dark-1 flex items-center justify-center p-4">
       <div className="w-full max-w-lg bg-dark-2 p-8 rounded-lg shadow-lg">
